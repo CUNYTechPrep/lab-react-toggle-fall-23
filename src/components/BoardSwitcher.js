41ea8cc4d176a5aaab77372f19d44947b9fc6230
Wei Jian Zhen <Wei Jian Zhen>
2023-09-20 11:13:06 -0400

    Completed lab.
    changed app to render 5 boards
    added state to track current selection
    added an event handler for the toggle button

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -6,22 +7,48 @@ function Board(props) {
     className += " selected";
   }
 
-  return <div className={className}>{props.index + 1}</div>;
+  return <div className={className}> {props.index + 1} </div>;
 }
 
 function BoardSwitcher(props) {
+
+  //Set state to 0;
+  const [state, setState] = useState(0);
+
+  /*
+  console.log("boards index is: " + boards[state].props.index);
+  console.log("boards selected is: " + boards[state].props.selected);
+  */
+
+  //boards array
   let boards = [];
-  for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
-    boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+
+  //Must get state referenced to a variable to be able to get selected to change index.
+  //let num get the initial value of state. 
+  let num = state;
+
+  //Reduce memory usage.
+  for (let i = 0; i < props.numBoards; i++) {
+    //let boolean variable isSelected get the current index of boards array and see if it matches num.
+    let isSelected = i === num;
+    boards.push(<Board index={i} selected={isSelected} key={i} />);
   }
+
+  function handleClick() {
+    //Update state of selected index to the next index. Wrap around back to the first board if over boards length.
+    setState((num) => (num + 1) % props.numBoards);
+
+    //console.log(num);
+  };
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+
+      <button onClick= {handleClick}>Toggle</button>
     </div>
   );
+
 }
 
 export default BoardSwitcher;

09e2cbeede179f6a09d38a5923d2d0fc85e67b2a
hninl23 <h.lwin0440@gmail.com>
2023-09-21 23:37:09 -0400

    completed the lab functionality

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,18 +10,26 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [selected, setSelected] = useState(0);
+
+  const handleToggle = () => {
+    const nextSelected = (selected + 1) % props.numBoards;
+    setSelected(nextSelected);
+  }
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selected;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleToggle}>Toggle</button>
     </div>
   );
 }
+
+
 
 export default BoardSwitcher;

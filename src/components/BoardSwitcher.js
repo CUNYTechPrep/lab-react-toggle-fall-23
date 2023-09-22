55048b281340cf494912454c61a277045cfe0a17
Tasneemhasanat97 <tasneem.hasanat01@qmail.cuny.edu>
2023-09-22 15:00:42 -0400

    added_handleClick

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -10,16 +10,26 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [selected, setSelected] = React.useState(0);
+
+
+
   let boards = [];
+
+  const handleClick = (event) => {  // added handleClick event to toggle boards
+    setSelected((selected + 1) % props.numBoards); // selected + 1 % numBoards will toggle between 0 and 1 or last board.
+  };
+
+
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selected;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}> Toggle</button>
     </div>
   );
 }

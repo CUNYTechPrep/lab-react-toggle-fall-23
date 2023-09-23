0fb4645d0ebc7b7c1a939438bbe69b1e656fbc1a
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-09-22 20:57:56 -0400

    udated readme and added 5 boards

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -17,11 +17,15 @@ function BoardSwitcher(props) {
 
   //onClick handler, update state + 1 and pass props.numBoards
   const handleClick = ()=> {
-    setState((getState + 1) %props.numBoards);
+    setState((getState + 1) %numBoards);
   }
 
+  //Modify numBoards so it shows 5 boards
+  const numBoards = props.numBoards + 2;
+
   let boards = [];
-  for (let ii = 0; ii < props.numBoards; ii++) {
+
+  for (let ii = 0; ii < numBoards; ii++) {
     let isSelected = ii === getState;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
